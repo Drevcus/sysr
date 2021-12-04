a8f30990956f527370b75d9e109523e629e7541f
Drevcus <dyron76@gmail.com>
2021-12-04 14:58:40 +0800

    Signed-off-by: Drevcus <dyron76@gmail.com>

--- dashboard.php
+++ dashboard.php
@@ -7,11 +7,40 @@
         header('location: login.php');
     }
 
-    echo $_SESSION['is_logged']['email'];
+    # echo $_SESSION['is_logged']['email'];
 ?>
 
 <h1><?php echo $page_title?></h1>
 <h2>Welcome <?php echo $_SESSION['is_logged']['fullname'];?></h2>
+<hr />
+<div class="container">
+	<div class="row">
+		<div class="col mb-2 mb-lg-0">
+			<div class="card">
+				<div class="card-header text-center">
+					<h5>Reports</h5>
+				</div>
+				<div class="card-body">
+					<p>test01</p>
+					<p>test02</p>
+				</div>
+			</div>
+		</div>
+		<div class="w-100 d-block d-lg-none"></div>
+		<div class="col mt-2 mt-lg-0">
+			<div class="card">
+				<div class="card-header text-center">
+					<h5>Responses</h5>
+				</div>
+				<div class="card-body">
+					<p>test01</p>
+					<p>test02</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
 <a href="logout.php">Logout</a>
 <?php
     include './layout/footer.php';
